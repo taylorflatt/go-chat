d4408e260eb8bb5004dcbba704555bec6b12ddbc
taylorjflatt@gmail.com <taylorjflatt@gmail.com>
2017-03-18 21:52:46 -0500

    Updated menu.
    Added the menu into methods inside its own class.

--- Client/client.go
+++ Client/client.go
@@ -7,7 +7,6 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -30,44 +29,34 @@ func RandInt32(min int32, max int32) int32 {
 	return min + rand.Int31n(max-min)
 }
 
-func MainMenu() {
-	addSpacing(1)
-	fmt.Println("Welcome to Go-Chat!")
-	addSpacing(1)
-	fmt.Println("Below is a list of menu options for the chat application.")
-	addSpacing(1)
-	fmt.Println("1) Create a Group")
-	fmt.Println("2) View Group List")
-	fmt.Println("3) Exit Chat")
-	addSpacing(1)
-	fmt.Print("Menu> ")
-}
-
-func GroupMenu() {
-	addSpacing(1)
-	fmt.Println("View Groups Menu")
-	addSpacing(1)
-	fmt.Println("Below is a list of menu options for groups.")
-	addSpacing(1)
-	fmt.Println("1) Join a Group")
-	fmt.Println("2) View Group Members")
-	fmt.Println("3) Refresh Group List")
-	fmt.Println("4) Go back")
-	addSpacing(1)
-	fmt.Print("Menu> ")
-}
-
-func ViewGroupMembersMenu() {
-	addSpacing(1)
-	fmt.Println("Enter the group name that you would like to view! Enter !back to go back to the menu.")
-	addSpacing(1)
-	fmt.Print("Menu> ")
-}
-
 func CheckError(err error) {
 	if err != nil {
 		fmt.Print(err)
 	}
+}
+
+func ExitChat(c pb.ChatClient, uName string) {
+	c.UnRegister(context.Background(), &pb.ClientInfo{Sender: uName})
+	os.Exit(1)
+}
+
+func ControlExitEarly(w chan os.Signal, c pb.ChatClient, uName string) {
+
+	signal.Notify(w, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-w
+	fmt.Print(sig)
+	fmt.Println(" used.")
+	fmt.Println("Exiting chat application.")
+
+	if sig == os.Interrupt {
+		ExitChat(c, uName)
+	}
+}
+
+func ControlExitLate(w chan os.Signal, c pb.ChatClient, uName string) {
+
+	ExitChat(c, uName)
 }
 
 func main() {
@@ -82,7 +71,6 @@ func main() {
 	// Read the server address
 	// DEBUG ONLY:
 	address := "localhost:12021"
-
 	// UNCOMMENT AFTER DEBUG
 	//fmt.Print("Please specify the server IP: ")
 	//t, _ := r.ReadString('\n')
@@ -121,166 +109,11 @@ func main() {
 
 		if err == nil {
 			fmt.Println("Your username: " + uName)
-
 			w := make(chan os.Signal, 1)
-
-			signal.Notify(w, syscall.SIGINT, syscall.SIGTERM)
-
-			go func() {
-				sig := <-w
-				fmt.Print(sig)
-				fmt.Print(" used.")
-				fmt.Println("Exiting chat application.")
-
-				if sig == os.Interrupt {
-					c.UnRegister(context.Background(), &pb.ClientInfo{Sender: uName})
-
-					os.Exit(1)
-				}
-			}()
+			go ControlExitEarly(w, c, uName)
 			break
 		} else {
 			fmt.Print(err)
-		}
-	}
-
-	rMainMenu := true
-	for rMainMenu {
-		MainMenu()
-		tc, err := r.ReadString('\n')
-		tc = strings.TrimSpace(tc)
-		CheckError(err)
-
-		choice, err := strconv.Atoi(tc)
-		CheckError(err)
-
-		if choice > 3 || choice < 1 {
-			fmt.Println("Please enter a valid selection between 1 and 3.")
-		} else if choice == 1 {
-			// Create a group
-
-			rCGroup := true
-			for rCGroup {
-				addSpacing(1)
-				fmt.Println("Enter the name of the group or type !back to go back to the main menu.")
-				fmt.Print("Menu> ")
-				gName, err = r.ReadString('\n')
-				gName = strings.TrimSpace(gName)
-				CheckError(err)
-
-				if gName == "!back" {
-					rCGroup = false
-				} else {
-					_, nerr := c.CreateGroup(context.Background(), &pb.GroupInfo{Client: uName, GroupName: gName})
-
-					if nerr != nil {
-						fmt.Println("That group name has already been chosen. Please select a new one.")
-					} else {
-						fmt.Println("Created group named " + gName)
-						c.JoinGroup(context.Background(), &pb.GroupInfo{Client: uName, GroupName: gName})
-						fmt.Println("Joined " + gName)
-						addSpacing(1)
-						rCGroup = false
-						rMainMenu = false
-					}
-				}
-			}
-
-		} else if choice == 2 {
-			// View groups
-			// List the current groups.
-			tr, _ := c.GetGroupList(context.Background(), &pb.Empty{})
-			res := tr.Groups
-
-			if len(res) == 0 {
-				fmt.Println("There are currently no groups created. Go back to the main menu to create one!")
-				addSpacing(1)
-			} else {
-				fmt.Println("List of all groups: ")
-				for i, gName := range res {
-					fmt.Println("  " + strconv.Itoa(i+1) + ") " + gName)
-				}
-			}
-
-			rGMenu := true
-			for rGMenu {
-				GroupMenu()
-				tgc, err := r.ReadString('\n')
-				tgc = strings.TrimSpace(tgc)
-				CheckError(err)
-
-				gChoice, err := strconv.Atoi(tgc)
-				CheckError(err)
-
-				if gChoice > 4 || gChoice < 1 {
-					fmt.Println("Please enter a valid selection between 1 and 4.")
-				} else if gChoice == 1 {
-					// Join a group
-					rGName := true
-					for rGName {
-						fmt.Println("Enter the name of the group as it appears in the group list or enter !back to go back to the Group menu.")
-						fmt.Print("menu> ")
-						gName, _ = r.ReadString('\n')
-						gName = strings.TrimSpace(gName)
-						CheckError(err)
-
-						if gName == "!back" {
-							rGName = false
-						} else {
-							_, err := c.JoinGroup(context.Background(), &pb.GroupInfo{Client: uName, GroupName: gName})
-							fmt.Println("Joined " + gName)
-
-							if err != nil {
-								fmt.Print("A group with that name doesn't exist. Please check again.")
-							} else {
-								rGName = false    // Leave join menu
-								rGMenu = false    // Leave Group menu
-								rMainMenu = false // Leave main menu
-							}
-						}
-					}
-
-				} else if gChoice == 2 {
-					//View Group members for a group
-					ViewGroupMembersMenu()
-
-					rGMemMenu := true
-					for rGMemMenu {
-						gCName, err := r.ReadString('\n')
-						gCName = strings.TrimSpace(gCName)
-						CheckError(err)
-
-						if gCName == "!back" {
-							rGMemMenu = false
-						} else {
-							cList, err := c.GetGroupClientList(context.Background(), &pb.GroupInfo{Client: uName, GroupName: gCName})
-							if err != nil {
-								fmt.Println("Please enter the group name exactly as it appears in the group list!")
-							} else {
-								fmt.Println(cList)
-								rGMemMenu = false
-							}
-						}
-					}
-				} else if gChoice == 3 {
-					// Refresh the group list
-					tr, _ := c.GetGroupList(context.Background(), &pb.Empty{})
-					res := tr.Groups
-
-					for _, gName := range res {
-						fmt.Println("Group: " + gName)
-					}
-
-				} else {
-					// Go back
-					addSpacing(4)
-					rGMenu = false
-				}
-			}
-		} else {
-			// Exit chat client
-			c.UnRegister(context.Background(), &pb.ClientInfo{Sender: uName})
-			os.Exit(0)
 		}
 	}
 
